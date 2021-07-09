52320114d5992d178ee49252a6917159235b89f3
TakafumiHiga <taka239133@icloud.com>
2021-07-09 14:38:10 +0900

    add_code

--- functions.php
+++ functions.php
@@ -13,3 +13,22 @@ add_image_size('large_thumbnail', 600, 400, true); // クロップモード
 add_action('after_setup_theme', 'my_theme_setup');
 
 }
+
+/* 投稿アーカイブを有効にしてスラッグを指定する */
+function post_has_archive( $args, $post_type ) {
+
+  if ( 'post' == $post_type ) {
+      $args['rewrite'] = true;
+      $args['has_archive'] = 'archive'; // スラッグ名
+   
+  }
+  return $args;
+}
+add_filter( 'register_post_type_args', 'post_has_archive', 10, 2 );
+
+// カスタムメニューを追加
+add_theme_support('menus');
+
+register_nav_menus ( array(
+ 'global' => 'グローバル',
+));
--- header.php
+++ header.php
@@ -14,17 +14,41 @@
 
 <body>
   <header>
-    <div class="globalmenu">
+    <nav class="globalmenu">
+
+
       <a href="/" class="logo globalmenu__logo"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"
           alt="ロゴイメージ画像" /></a>
-      <ul class="globalmenu__list">
+      <?php 
+            wp_nav_menu(array(
+            'container_class' => 'globalmenu__list-container', 
+            'menu_class' => 'globalmenu__list',//ulのclass
+            'menu_id'         => 'globalmenu__list', //ulのid
+            'theme_location' => 'global',
+            'container' => 'nav',
+            'depth' => 3,
+            
+           
+            ));
+            ?>
+
+      <!-- <ul class="globalmenu__list">
         <li class="menu-item"><a href="/">ホーム</a></li>
         <li class="menu-item"><a href="/">News</a></li>
-      </ul>
-    </div>
+      </ul> -->
+    </nav>
+
     <div class="key__visual">
+      <!-- 条件分岐 -->
+
+      <?php if (is_home() || is_front_page() ): ?>
+
       <h1 class="key__visual-header">Practice-Blog</h1>
       <img src="<?php echo get_template_directory_uri(); ?>/img/img4-hill.jpg" alt="丘と太陽が登る画像" />
-    </div>
 
+      <?php elseif ( is_archive()) : ?>
+      <h1 class="key__visual-header">Archive</h1>
+      <img src="<?php echo get_template_directory_uri(); ?>/img/img3-cat.jpg" alt="子猫が木に登っている画像" />
+    </div>
+    <?php endif; ?>
     <!-- ここまでheader.phpへ -->
