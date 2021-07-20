93a012d4ba284f60c523945f3df57e4e56777c4a
TakafumiHiga <taka239133@icloud.com>
2021-07-20 14:17:26 +0900

    add_item-post

---
+++ archive.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+</header>
+<main>
+  <section id="news" class="news">
+    <h2 class="section__header news__header">アーカイブです。</h2>
+    <div class="post__item-wrap">
+      <ul class="post__item-list">
+        <!-- メインループをここに記入 -->
+        <?php if (have_posts()): while (have_posts()): the_post(); ?>
+        <li class="post__item">
+          <!-- パーマリンクを追記 -->
+          <a href="<?php the_permalink(); ?>/">
+            <figure class="post__item-img">
+              <!-- アイキャッチ画像を表示する -->
+              <?php if (has_post_thumbnail()): the_post_thumbnail('');
+            
+          else: ?>
+              <!-- アイキャッチ画像がない場合 -->
+              <img src="<?php echo get_template_directory_uri(); ?>/img/img2-bill.jpg" alt="" />
+              <!-- アイキャッチ画像のphp閉じ -->
+              <?php 
+                endif;
+              ?>
+            </figure>
+            <div class="post__item-text-wrap">
+              <span class="post__item-date"><?php the_time('Y.m.j'); ?></span><span class="post__item-cat">自然</span>
+              <h3 class="post__item-title">
+                <?php the_title(); ?>
+              </h3>
+              <div class="post__item-desc">
+                <?php the_content(); ?>
+              </div>
+            </div>
+            <?php endwhile; else: ?>
+            <?php endif; ?>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </section>
+</main>
+<footer class="footer">
+  <h2 class="footer__heading">やんばるエキスパート<br />WordPress勉強会</h2>
+</footer>
+<!-- ゲットフッター -->
+<?php wp_footer(); ?>
+</body>
+
+</html>
--- functions.php
+++ functions.php
@@ -13,3 +13,78 @@ add_image_size('large_thumbnail', 600, 400, true); // クロップモード
 add_action('after_setup_theme', 'my_theme_setup');
 
 }
+
+/* 投稿アーカイブを有効にしてスラッグを指定する */
+function post_has_archive( $args, $post_type ) {
+
+  if ( 'post' == $post_type ) {
+      $args['rewrite'] = true;
+      $args['has_archive'] = 'archive'; // スラッグ名
+   
+  }
+  return $args;
+}
+add_filter( 'register_post_type_args', 'post_has_archive', 10, 2 );
+
+// カスタムメニューを追加
+add_theme_support('menus');
+
+register_nav_menus ( array(
+ 'global' => 'グローバル',
+));
+
+//カスタム投稿を追加
+
+
+function cpt_add_item() { //add_actionの２つのパラメーターを定義
+	$labels = [
+		"singular_name" => "item",
+		"edit_item" => "item",
+	];
+	$args = [  //配列を定義
+		"label" => "商品", //管理画面に出てくる名前
+		"labels" => $labels,
+		"description" => "商品一覧",//投稿タイプの簡潔な説明
+		"public" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => true, //アーカイブページ
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"map_meta_cap" => true,
+		"hierarchical" => true,//親子関係
+		
+		"rewrite" => [ "slug" => "item", "with_front" => true ], //スラッグをitemに設定
+		"query_var" => true,
+		"menu_position" => 5,
+		"supports" => [ "title", "editor", "thumbnail", "customfields" ],
+		'menu_icon' => 'dashicons-store',//wp公式アイコン
+	];
+	register_post_type( "item", $args );//カスタム投稿タイプを追加する関数
+}
+add_action( 'init', 'cpt_add_item' );//init→特定の処理のときにこの処理を挟む。cpt_re
+
+//itemカスタム投稿にカテゴリーを追加
+
+function cpt_add_dep() { //add_actionの２つのパラメーターを定義
+	$labels = [
+		"singular_name" => "dep",
+	];
+	$args = [
+		"label" => "カテゴリー",
+		"labels" => $labels,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_in_menu" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'dep', 'with_front' => true, ], //カテゴリーのスラッグ
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"rest_base" => "dep",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"show_in_quick_edit" => false,
+		];
+	register_taxonomy( "dep", [ "item" ], $args ); //「item」というカスタム投稿タイプにカテゴリーを追加
+}
+add_action( 'init', 'cpt_add_dep' );
